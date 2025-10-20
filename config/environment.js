21c1eb319597668e90f14bd5b2588e532b5c1b29
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2025-10-20 22:50:23 +0200

    environment.js hash

--- config/environment.js
+++ config/environment.js
@@ -43,6 +43,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.rootURL = '/QuizKoenig/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
